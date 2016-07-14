ef2f326a245ee4d30bc79e3af2d14c5ab9e193a1
unknown <>
2016-07-14 18:03:29 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var postcss = require('postcss');
+var _postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
+var _import = require('postcss-import');
+var autoprefixer = require('autoprefixer');
+var csswring = require('csswring');
+var cssmqpacker = require('css-mqpacker');
+var px2rem = require('postcss-px2rem');
+var nested = require('postcss-nested');
+
+gulp.task('postcss', function(){
+  var processors = [
+    _import,
+    nested,
+    cssmqpacker,
+    csswring,
+    px2rem({remUnit: 75}),
+    autoprefixer
+  ];
+  return gulp.src('./css/main.css')
+    .pipe(_postcss(processors))
+    .on('error', errorHandler)
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./css/dest'))
+})
+
+gulp.task('watch', function(){
+  gulp.watch('./css/src/**/*.css', ['postcss']);
+})
+
+gulp.task('default', ['postcss', 'watch']);
+
+function errorHandler(error){
+  console.log(error.message);
+  console.log(error.fileName);
+  console.log('line:', error.line, 'column:', error.column);
+  this.emit('end');
+}
