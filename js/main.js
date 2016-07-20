29de8a8839108a6c5bfd1b64301816b34724bd2b
unknown <>
2016-07-20 16:33:54 +0800

    slidebar

--- js/main.js
+++ js/main.js
@@ -11,6 +11,27 @@ $(function(){
 
   var mySwiper = new Swiper('.swiper-container', {
     slidesPerView : 4,
-  })
+  });
+
+
+  +function(){
+    var $uiSlideCont = $('.ui-slide-cont');
+    var w = $uiSlideCont.width();
+    var o = {
+      min: 0,
+      max: 6
+    }
+    var init = function(){
+      $uiSlideCont.find('.ui-slider-handle').css('left',0);
+      $uiSlideCont.find('.ui-slider-range').css('width',0);
+      $uiSlideCont.find('.ui-slider-spot').empty()
+      for(var i=0; i<o.max-1; i++){
+        var $item = $('<div class="stalls-item"></div>');
+        $uiSlideCont.find('.ui-slider-spot').append($item)
+      }
+    }
+    init();
+
+  }()
 
 })
