1172746abbd115ad31401b395984cd662f241a38
unknown <>
2016-07-22 17:23:47 +0800

    uiSlideBar

--- js/main.js
+++ js/main.js
@@ -17,44 +17,44 @@ $(function(){
   +function(){
     var $uiSlideCont = $('.ui-slide-cont');
     var $uiBtn = $('.ui-slider-handle', $uiSlideCont);
-    var w = $uiSlideCont.width();
     var o = {
       min: 0,
       max: 6,
       desc: ['夜间', '低档', '中档', '高档', '喷射', '自动']
     }
 
-    // e.originalEvent.changedTouches[0];
-    var Width = $uiSlideCont.width();
+    var Width = $uiSlideCont.width();  //注意使用$.css('width')与$.width()的区别
     var moveStart,move,moveEnd;
     var winFont = parseInt($('html').css('fontSize'));
     var ml = $('.ui-slider-bar').position().left;  //距离左边的距离
 
-
-    $uiSlideCont.on('touchstart', '.ui-slider-spot', function(e){
+    $uiBtn.find('span').text(o.desc[0]);
+    //touchstart/touchend/touchmove
+    $uiSlideCont.on('touchstart', '.ui-slider-bar', function(e){
       var g = e.originalEvent.touches[0];
       var s = range(g.pageX - ml);
-      $uiBtn.stop(true, true).animate({left: s}, 300);
+      $uiBtn.stop(true, true).animate({left: correct(s).left}, 300);
+      $uiBtn.find('span').text(o.desc[correct(s).site])
+
     }).on('touchstart', '.ui-slider-handle', function(e){
       var g = e.originalEvent.touches[0];
-      console.log(g.pageX, 'handle');
       moveStart = g.pageX - ml;
     }).on('touchmove', '.ui-slider-handle', function(e){
-      console.log(moveEnd-moveStart);
       var g = e.originalEvent.touches[0];
       moveEnd = g.pageX - ml;
       var distance = range(moveEnd);
-
       var _this = $(this);
-      _this.stop(true,true).animate({left: distance}, 50)
-      // event.preventDefault();
+      _this.stop(true,true).animate({left: distance}, 0);
+      _this.find('span').text(o.desc[correct(distance).site])
+      event.preventDefault();
     }).on('touchend', '.ui-slider-handle', function(e){
       var g = e.originalEvent.changedTouches[0];
       moveEnd = g.pageX - ml;
       var distance = range(moveEnd);
       var _this = $(this);
-      _this.stop(true,true).animate({left: distance}, 50)
-
+      _this.stop(true,true).animate({left: correct(distance).left}, 300);
+      _this.find('span').text(o.desc[correct(distance).site])
+      event.preventDefault();
     })
 
     var init = function(){
@@ -62,13 +62,17 @@ $(function(){
       $uiSlideCont.find('.ui-slider-range').css('width',0);
       $uiSlideCont.find('.ui-slider-spot').empty();
       for(var i=0; i<o.max-1; i++){
-        var $item = $('<div class="stalls-item"></div>');
+        var $item = $('<div class="stalls-item pit"><b></b></div>');
+        if(i==0){
+          $item.addClass('stalls-item-first').append('<b class="b1"></b>');
+        }
         $uiSlideCont.find('.ui-slider-spot').append($item);
       };
       for(var i=0; i<o.desc.length; i++){
         var $itemDesc = $('<div class="ui-slider-desc">'+o.desc[i]+'</div>')
         $uiSlideCont.find('.ui-slider-xlabel').append($itemDesc);
       }
+
     }
     init();
 
@@ -80,8 +84,24 @@ $(function(){
         d = Width;
       }
       // console.log(d/winFont);
-      return (d/winFont) +'rem';
+      return d;
     }
+    //就近归位
+    function correct(d){
+      if(!d){
+        d = 0;
+      }
+      var len = $('.ui-slider-spot').children().length; //存在几档
+      var w = Width/len; //单位长度
+      var integer = (d/w).toFixed(2);
+      var t = Math.round(integer);
+      var o = {
+        left: ((t*w)/winFont).toFixed(6) + 'rem',
+        site: t
+      }
+      return o;
+    }
+
   }()
 
 })
--- js/ui-slide-bar.js
+++ js/ui-slide-bar.js
@@ -1,3 +1,31 @@
-var uiSlideBar = {
-  //常量
-}
++function(){
+  var uiSlideBar = function(elem){
+    this.$element = $(elem);
+    this.defaults = {
+      minNum: 0,
+      maxNum: 6,
+      desc: ['夜间', '低档', '中档', '高档', '喷射', '自动'],  //
+      btn: {
+        color: '#109fff'
+      },
+      ininialDuang: 0,  //初始档位
+      nowTip: true, //btn-tips show
+
+    };
+    this.options = $.extend({}, default, opt)
+  }
+
+  //定义原型方法
+  uiSlideBar.prototype = {
+
+  }
+
+
+  $.fn.uiSlideBar = function(elem, opts){
+     //init
+
+    //Event
+
+    //Render
+  }
+}()
