a9af2120fbfc4608c7caf20da09efdd5ad3f0da9
codedream <codedreamfy@outlook.com>
2016-07-25 00:08:07 +0800

    uisliderbar

--- js/ui-slide-bar.js
+++ js/ui-slide-bar.js
@@ -1,24 +1,28 @@
 +function(){
-  var uiSlideBar = function(elem){
+  var UiSlideBar = function(elem){
     this.$element = $(elem);
     this.defaults = {
       minNum: 0,
       maxNum: 6,
-      desc: ['夜间', '低档', '中档', '高档', '喷射', '自动'],  //
+      desc: [],  //['夜间', '低档', '中档', '高档', '喷射', '自动']
       btn: {
         color: '#109fff'
       },
       ininialDuang: 0,  //初始档位
       nowTip: true, //btn-tips show
+      pit: true, //small kedu
+      progress: false // defalut: false;
 
     };
-    this.options = $.extend({}, default, opt)
+    this.options = $.extend({}, default, opt || {})
   }
 
   //定义原型方法
-  uiSlideBar.prototype = {
-
-  }
+  $.extend(UiSlideBar.prototype, {
+    _init: {},
+    _render: {},
+    _bindEvent: {}
+  })
 
 
   $.fn.uiSlideBar = function(elem, opts){
