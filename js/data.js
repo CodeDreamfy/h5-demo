e0b164c08355dd2ffc5b45790cb2da07d60cce66
unknown <>
2016-08-02 10:01:27 +0800

    develop

---
+++ js/data.js
@@ -0,0 +1,114 @@
+//data
+var eq1 = [
+  {
+    desc: ['-10', '-5', '0', '5', '10', '15', '20', '25', '30'],
+    initialDuang: -5,
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  },
+  {
+    initialDuang: '3档',
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  },
+  {
+    desc: ['制冷', '制热', '自动', '除湿', '送风', '加湿'],
+    initialDuang: '自动',
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  }
+];
+var eq2 = [
+  {
+    desc: ['-10', '-5', '0', '5', '10', '15', '20', '25', '30'],
+    initialDuang: 0,
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  },
+  {
+    initialDuang: '3档',
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  },
+  {
+    desc: ['制冷', '制热', '自动', '除湿', '送风', '加湿'],
+    initialDuang: '制热',
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  }
+];
+var eq3 = [
+  {
+    desc: ['-10', '-5', '0', '5', '10', '15', '20', '25', '30'],
+    initialDuang: 20,
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  },
+  {
+    initialDuang: '1档',
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  },
+  {
+    desc: ['制冷', '制热', '自动', '除湿', '送风', '加湿'],
+    initialDuang: '送风',
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  }
+];
+var eq4 = [
+  {
+    desc: ['-10', '-5', '0', '5', '10', '15', '20', '25', '30'],
+    initialDuang: 5,
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  },
+  {
+    initialDuang: '4档',
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  },
+  {
+    desc: ['制冷', '制热', '自动', '除湿', '送风', '加湿'],
+    initialDuang: '加湿',
+    allowSlide: false,
+    progress: false,
+    descShow: false,
+    spotShow: false
+  }
+];
+var eq5 = [
+  {
+    desc: ['-10', '-5', '0', '5', '10', '15', '20', '25', '30'],
+    initialDuang: 0
+  },
+  {
+    initialDuang: '4档'
+  },
+  {
+    desc: ['制冷', '制热', '自动', '除湿', '送风', '加湿'],
+    initialDuang: '自动'
+  }
+];
